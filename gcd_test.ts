766efb6574b7b259a60fc8ff403c00d9145cc2df
Luka Vucer <vucerluka@gmail.com>
2026-03-27 09:29:36 +0100

    erster gcd test

--- gcd_test.ts
+++ gcd_test.ts
@@ -1,2 +1,16 @@
 import { gcdBruteForce } from "./gcd";
+import {assertEquals } from "@std/assert";
+
+Deno.Test("Von 18/27 ist der ggT 9", () => {
+
+    const a:number = 18;
+    const b:number = 27;
+    
+
+    const i = gcdBruteForce(a, b,);
+    
+    return i
+
+
+});
 
