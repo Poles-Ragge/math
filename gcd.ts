9ebaec178d5be3adc1e8e8327f4684de8832e429
Luka Vucer <vucerluka@gmail.com>
2026-03-27 09:15:01 +0100

    erster gcd function

--- gcd.ts
+++ gcd.ts
@@ -2,4 +2,14 @@
 
 export function gcdBruteForce(a: number, b: number): number{
 
+    var i:number = 2;
+
+    while (a / i != b / i ){
+        if (a % i == 0 || b % i == 0){
+            i++
+        }
+        
+
+    }
+    return i;
 }
--- gcd_test.ts
+++ gcd_test.ts
@@ -1,1 +1,2 @@
 import { gcdBruteForce } from "./gcd";
+
